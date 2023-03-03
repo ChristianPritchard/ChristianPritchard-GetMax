355ff8f2d070934727a23e5ea47727fc59540245
ChristianPritchard <christian.pritchard14@gmail.com>
2023-03-03 22:53:41 +0000

    getmax lab complete

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,18 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max = arr[0];
+
+        //skipping checking first element of the array since we have already assigned it to a variable 
+        for(int i = 1; i < arr.length; i++){
+
+            if(arr[i] > max){
+
+                max = arr[i];
+
+            }
+        }
+
+        return max;
     }
 }
